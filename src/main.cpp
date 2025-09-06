38a44da59b1ebf25076a7fc17dd0def529a737ba
LetMeL1ve <roman.null13@gmail.com>
2025-09-06 20:04:39 +0300

    Added gameplay

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 const int field_size = 3;
 char field[field_size][field_size] = {
     {'1', '2', '3'},
-    {'X', 'X', 'X'},
+    {'4', '5', '6'},
     {'7', '8', '9'}
 };
+bool is_x_playing = false;
 
 void print_field();
 
-bool field_check();
+bool check_field();
+
+void player_move();
+
+char get_number_as_char();
 
 int main()
 {
-   print_field();
-   std::cout << std::boolalpha << field_check();
+    while (check_field()) {
+        print_field();
+        player_move();
+    }
+    std::cout << (is_x_playing ? "O wins!" : "X wins!");
 }
 
 void print_field() {
@@ -31,7 +40,7 @@ void print_field() {
     }
 }
 
-bool field_check() { // returns false if someone wins.
+bool check_field() { // returns false if someone wins.
     if (field[0][0] == field[0][1] && field[0][2] == field[0][1])
     return false;
     else if (field[1][0] == field[1][1] && field[1][2] == field[1][1])
@@ -43,4 +52,26 @@ bool field_check() { // returns false if someone wins.
     else if (field[0][2] == field[1][1] && field[2][0] == field[1][1])
     return false;
     return true;
+}
+char get_number_as_char() {
+    char number;
+    while (true) {
+        std::cout << "Enter number:";
+        if (std::cin >> number)
+            return number;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+void player_move() {
+    char number = get_number_as_char();
+    for (int y = 0; y != std::size(field); ++y) {
+        for (int x = 0; x != std::size(field); ++x) {
+            if ((char)field[y][x] == (char)number) {
+                field[y][x] = is_x_playing ? 'X' : 'O';
+                is_x_playing = !is_x_playing;
+                return;
+            }
+        }
+    }
 }
