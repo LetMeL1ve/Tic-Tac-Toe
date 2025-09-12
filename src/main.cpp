2029691adced63f9852c760109b687d7a67cf934
LetMeL1ve <roman.null13@gmail.com>
2025-09-12 17:17:33 +0300

    Adde main_game_logic.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include "main_game_logic.h"
-using namespace main_game_logic;
 
 int main()
 {
-    while (check_field() == 0) {
-        print_field();
-        player_move();
+    char field[main_game_logic::field_size][main_game_logic::field_size] = { // Array that represents a game field.
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+    
+    while (main_game_logic::check_field(field) == 0) {
+        main_game_logic::print_field(field);
+        main_game_logic::player_move(field);
     }
-    std::cout << (is_x_playing ? "O wins!" : "X wins!");
+    std::cout << (main_game_logic::is_x_playing ? "O wins!" : "X wins!");
 }
